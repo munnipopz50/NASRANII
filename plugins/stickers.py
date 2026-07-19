275e5bc4df07b269203a98e88a13d55d90fe838d
munnipopz50 <munnipopz50@gmail.com>
2026-07-19 20:24:34 +0530

    Update stickers.py

--- plugins/stickers.py
+++ plugins/stickers.py
@@ -1,245 +1,304 @@
-import os
-import requests
-from PIL import Image
-from pyrogram import Client, filters
+from io import BytesIO
+from pyrogram import Client, filters, enums
+from pyrogram.types import Message
+from httpx import AsyncClient, Timeout
+from plugins.helpers.config import *
+from info import API_ID, API_HASH, BOT_TOKEN
+import logging
 from info import *
-# ================= CONFIG =================
 
 
-# ================= STORAGE =================
-TEMP_FILES = {}
-USER_PACKS = {}
 
-# ================= SAVE FILE =================
-@Client.on_message(
-    filters.private &
-    (
-        filters.photo |
-        filters.document |
-        filters.video
-    )
+
+
+fetch = AsyncClient(
+    http2=True,
+    verify=False,
+    headers={
+        "Accept-Language": "id-ID",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.42",
+    },
+    timeout=Timeout(20),
 )
-async def save_file(client, message):
 
-    msg = await message.reply_text(
-        "📥 Downloading..."
-    )
 
-    file_path = await message.download()
+class QuotlyException(Exception):
+    pass
 
-    file_type = None
+app = ()
 
-    # ================= PHOTO =================
-    if message.photo:
 
-        new_path = (
-            f"{message.from_user.id}.png"
-        )
-
-        # Open image
-        img = Image.open(file_path)
-
-        # Resize image
-        img.thumbnail((512, 512))
-
-        # Save PNG
-        img.save(
-            new_path,
-            "PNG"
-        )
-
-        try:
-            os.remove(file_path)
-        except:
-            pass
-
-        file_path = new_path
-
-        file_type = "photo"
-
-    # ================= VIDEO =================
-    elif file_path.endswith(".webm"):
-
-        file_type = "video"
-
+async def get_message_sender_id(ctx: Message):
+    if ctx.forward_date:
+        if ctx.forward_sender_name:
+            return 1
+        elif ctx.forward_from:
+            return ctx.forward_from.id
+        elif ctx.forward_from_chat:
+            return ctx.forward_from_chat.id
+        else:
+            return 1
+    elif ctx.from_user:
+        return ctx.from_user.id
+    elif ctx.sender_chat:
+        return ctx.sender_chat.id
     else:
+        return 1
 
-        try:
-            os.remove(file_path)
-        except:
-            pass
 
-        return await msg.edit_text(
-            "❌ Only Photo or .webm supported."
+async def get_message_sender_name(ctx: Message):
+    if ctx.forward_date:
+        if ctx.forward_sender_name:
+            return ctx.forward_sender_name
+        elif ctx.forward_from:
+            return (
+                f"{ctx.forward_from.first_name} {ctx.forward_from.last_name}"
+                if ctx.forward_from.last_name
+                else ctx.forward_from.first_name
+            )
+
+        elif ctx.forward_from_chat:
+            return ctx.forward_from_chat.title
+        else:
+            return ""
+    elif ctx.from_user:
+        if ctx.from_user.last_name:
+            return f"{ctx.from_user.first_name} {ctx.from_user.last_name}"
+        else:
+            return ctx.from_user.first_name
+    elif ctx.sender_chat:
+        return ctx.sender_chat.title
+    else:
+        return ""
+
+
+async def get_custom_emoji(ctx: Message):
+    if ctx.forward_date:
+        return (
+            ""
+            if ctx.forward_sender_name
+            or not ctx.forward_from
+            and ctx.forward_from_chat
+            or not ctx.forward_from
+            else ctx.forward_from.emoji_status.custom_emoji_id
         )
 
-    TEMP_FILES[
-        message.from_user.id
-    ] = {
-        "path": file_path,
-        "type": file_type
-    }
+    return ctx.from_user.emoji_status.custom_emoji_id if ctx.from_user else ""
 
-    await msg.edit_text(
-        "✅ File Saved\n\n"
-        "Now send:\n"
-        "/kang PackName"
-    )
 
-# ================= CREATE PACK =================
-@Client.on_message(
-    filters.command("kang")
-)
-async def kang_pack(client, message):
+async def get_message_sender_username(ctx: Message):
+    if ctx.forward_date:
+        if (
+            not ctx.forward_sender_name
+            and not ctx.forward_from
+            and ctx.forward_from_chat
+            and ctx.forward_from_chat.username
+        ):
+            return ctx.forward_from_chat.username
+        elif (
+            not ctx.forward_sender_name
+            and not ctx.forward_from
+            and ctx.forward_from_chat
+            or ctx.forward_sender_name
+            or not ctx.forward_from
+        ):
+            return ""
+        else:
+            return ctx.forward_from.username or ""
+    elif ctx.from_user and ctx.from_user.username:
+        return ctx.from_user.username
+    elif (
+        ctx.from_user
+        or ctx.sender_chat
+        and not ctx.sender_chat.username
+        or not ctx.sender_chat
+    ):
+        return ""
+    else:
+        return ctx.sender_chat.username
 
-    user_id = message.from_user.id
 
-    if user_id not in TEMP_FILES:
+async def get_message_sender_photo(ctx: Message):
+    if ctx.forward_date:
+        if (
+            not ctx.forward_sender_name
+            and not ctx.forward_from
+            and ctx.forward_from_chat
+            and ctx.forward_from_chat.photo
+        ):
+            return {
+                "small_file_id": ctx.forward_from_chat.photo.small_file_id,
+                "small_photo_unique_id": ctx.forward_from_chat.photo.small_photo_unique_id,
+                "big_file_id": ctx.forward_from_chat.photo.big_file_id,
+                "big_photo_unique_id": ctx.forward_from_chat.photo.big_photo_unique_id,
+            }
+        elif (
+            not ctx.forward_sender_name
+            and not ctx.forward_from
+            and ctx.forward_from_chat
+            or ctx.forward_sender_name
+            or not ctx.forward_from
+        ):
+            return ""
+        else:
+            return (
+                {
+                    "small_file_id": ctx.forward_from.photo.small_file_id,
+                    "small_photo_unique_id": ctx.forward_from.photo.small_photo_unique_id,
+                    "big_file_id": ctx.forward_from.photo.big_file_id,
+                    "big_photo_unique_id": ctx.forward_from.photo.big_photo_unique_id,
+                }
+                if ctx.forward_from.photo
+                else ""
+            )
 
-        return await message.reply_text(
-            "❌ Send photo or .webm first."
-        )
-
-    if len(message.command) < 2:
-
-        return await message.reply_text(
-            "Usage:\n/kang PackName"
-        )
-
-    pack_title = message.command[1]
-
-    data = TEMP_FILES[user_id]
-
-    file_path = data["path"]
-
-    file_type = data["type"]
-
-    msg = await message.reply_text(
-        "📤 Creating Sticker Pack..."
-    )
-
-    try:
-
-        bot_username = (
-            await client.get_me()
-        ).username
-
-        short_name = (
-            f"{pack_title}_{user_id}"
-            f"_by_{bot_username}"
-        ).lower()
-
-        url = (
-            f"https://api.telegram.org/bot"
-            f"{BOT_TOKEN}/createNewStickerSet"
-        )
-
-        payload = {
-            "user_id": user_id,
-            "name": short_name,
-            "title": pack_title,
-            "emojis": "🔥"
+    elif ctx.from_user and ctx.from_user.photo:
+        return {
+            "small_file_id": ctx.from_user.photo.small_file_id,
+            "small_photo_unique_id": ctx.from_user.photo.small_photo_unique_id,
+            "big_file_id": ctx.from_user.photo.big_file_id,
+            "big_photo_unique_id": ctx.from_user.photo.big_photo_unique_id,
+        }
+    elif (
+        ctx.from_user
+        or ctx.sender_chat
+        and not ctx.sender_chat.photo
+        or not ctx.sender_chat
+    ):
+        return ""
+    else:
+        return {
+            "small_file_id": ctx.sender_chat.photo.small_file_id,
+            "small_photo_unique_id": ctx.sender_chat.photo.small_photo_unique_id,
+            "big_file_id": ctx.sender_chat.photo.big_file_id,
+            "big_photo_unique_id": ctx.sender_chat.photo.big_photo_unique_id,
         }
 
-        # ================= PHOTO STICKER =================
-        if file_type == "photo":
 
-            with open(file_path, "rb") as sticker:
+async def get_text_or_caption(ctx: Message):
+    if ctx.text:
+        return ctx.text
+    elif ctx.caption:
+        return ctx.caption
+    else:
+        return ""
 
-                response = requests.post(
-                    url,
-                    data=payload,
-                    files={
-                        "png_sticker": sticker
-                    }
-                )
 
-        # ================= VIDEO STICKER =================
-        else:
+async def pyrogram_to_quotly(messages, is_reply):
+    if not isinstance(messages, list):
+        messages = [messages]
+    payload = {
+        "type": "quote",
+        "format": "png",
+        "backgroundColor": "#1b1429",
+        "messages": [],
+    }
 
-            with open(file_path, "rb") as sticker:
-
-                response = requests.post(
-                    url,
-                    data=payload,
-                    files={
-                        "webm_sticker": sticker
-                    }
-                )
-
-        result = response.json()
-
-        # ================= SUCCESS =================
-        if result.get("ok"):
-
-            if user_id not in USER_PACKS:
-                USER_PACKS[user_id] = []
-
-            USER_PACKS[user_id].append(
+    for message in messages:
+        the_message_dict_to_append = {}
+        if message.entities:
+            the_message_dict_to_append["entities"] = [
                 {
-                    "title": pack_title,
-                    "short": short_name
+                    "type": entity.type.name.lower(),
+                    "offset": entity.offset,
+                    "length": entity.length,
                 }
-            )
-
-            await msg.edit_text(
-                f"✅ Sticker Pack Created!\n\n"
-                f"https://t.me/addstickers/"
-                f"{short_name}"
-            )
-
+                for entity in message.entities
+            ]
+        elif message.caption_entities:
+            the_message_dict_to_append["entities"] = [
+                {
+                    "type": entity.type.name.lower(),
+                    "offset": entity.offset,
+                    "length": entity.length,
+                }
+                for entity in message.caption_entities
+            ]
         else:
-
-            await msg.edit_text(
-                f"❌ Error:\n"
-                f"{result}"
-            )
-
-    except Exception as e:
-
-        await msg.edit_text(
-            f"❌ Error:\n{e}"
+            the_message_dict_to_append["entities"] = []
+        the_message_dict_to_append["chatId"] = await get_message_sender_id(message)
+        the_message_dict_to_append["text"] = await get_text_or_caption(message)
+        the_message_dict_to_append["avatar"] = True
+        the_message_dict_to_append["from"] = {}
+        the_message_dict_to_append["from"]["id"] = await get_message_sender_id(message)
+        the_message_dict_to_append["from"]["name"] = await get_message_sender_name(
+            message
         )
+        the_message_dict_to_append["from"][
+            "username"
+        ] = await get_message_sender_username(message)
+        the_message_dict_to_append["from"]["type"] = message.chat.type.name.lower()
+        the_message_dict_to_append["from"]["photo"] = await get_message_sender_photo(
+            message
+        )
+        if message.reply_to_message and is_reply:
+            the_message_dict_to_append["replyMessage"] = {
+                "name": await get_message_sender_name(message.reply_to_message),
+                "text": await get_text_or_caption(message.reply_to_message),
+                "chatId": await get_message_sender_id(message.reply_to_message),
+            }
+        else:
+            the_message_dict_to_append["replyMessage"] = {}
+        payload["messages"].append(the_message_dict_to_append)
+    r = await fetch.post("https://bot.lyo.su/quote/generate.png", json=payload)
+    if not r.is_error:
+        return r.read()
+    else:
+        raise QuotlyException(r.json())
 
-    # ================= CLEANUP =================
+
+def isArgInt(txt) -> list:
+    count = txt
     try:
-        os.remove(file_path)
-    except:
-        pass
+        count = int(count)
+        return [True, count]
+    except ValueError:
+        return [False, 0]
 
-    TEMP_FILES.pop(user_id, None)
 
-# ================= PACK LIST =================
-@Client.on_message(
-    filters.command("packs")
-)
-async def packs_list(client, message):
-
-    user_id = message.from_user.id
-
-    if user_id not in USER_PACKS:
-
-        return await message.reply_text(
-            "❌ No packs found."
+@Client.on_message(filters.command(["q", "quotly"]) & filters.reply)
+async def msg_quotly_cmd(self: app, ctx: Message):
+    is_reply = False
+    if ctx.command[0].endswith("r"):
+        is_reply = True
+    if len(ctx.text.split()) > 1:
+        check_arg = isArgInt(ctx.command[1])
+        if check_arg[0]:
+            if check_arg[1] < 2 or check_arg[1] > 10:
+                return await ctx.reply_msg("Invalid range", del_in=6)
+            try:
+                messages = [
+                    i
+                    for i in await self.get_messages(
+                        chat_id=ctx.chat.id,
+                        message_ids=range(
+                            ctx.reply_to_message.id,
+                            ctx.reply_to_message.id + (check_arg[1] + 5),
+                        ),
+                        replies=-1,
+                    )
+                    if not i.empty and not i.media
+                ]
+            except Exception:
+                return await ctx.reply_text("🤷🏻‍♂️")
+            try:
+                make_quotly = await pyrogram_to_quotly(messages, is_reply=is_reply)
+                bio_sticker = BytesIO(make_quotly)
+                bio_sticker.name = "MINNALMURALI_STICKER.webp"
+                return await ctx.reply_sticker(bio_sticker)
+            except Exception:
+                return await ctx.reply_msg("🤷🏻‍♂️")
+    try:
+        messages_one = await self.get_messages(
+            chat_id=ctx.chat.id, message_ids=ctx.reply_to_message.id, replies=-1
         )
-
-    text = "📦 Your Sticker Packs\n\n"
-
-    for num, pack in enumerate(
-        USER_PACKS[user_id],
-        start=1
-    ):
-
-        text += (
-            f"{num}. "
-            f"{pack['title']}\n"
-            f"https://t.me/addstickers/"
-            f"{pack['short']}\n\n"
-        )
-
-    await message.reply_text(
-        text,
-        disable_web_page_preview=True
-    )
-
-# ================= START BOT =================
+        messages = [messages_one]
+    except Exception:
+        return await ctx.reply_msg("🤷🏻‍♂️")
+    try:
+        make_quotly = await pyrogram_to_quotly(messages, is_reply=is_reply)
+        bio_sticker = BytesIO(make_quotly)
+        bio_sticker.name = "MINNALMURALI_STICKER.webp"
+        return await ctx.reply_sticker(bio_sticker)
+    except Exception as e:
+        return await ctx.reply_msg(f"ERROR: {e}")
