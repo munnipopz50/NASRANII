089b72b46ad0b7bd1336be0698d339d8fc0cd93b
munnipopz50 <munnipopz50@gmail.com>
2026-07-20 19:51:18 +0530

    Update stickers.py

--- plugins/stickers.py
+++ plugins/stickers.py
@@ -2,7 +2,10 @@ from pyrogram import Client, filters
 
 
 print("✅ STICKERS PLUGIN LOADED")
+print("=== STICKER IMPORTED ===")
+
+
+
 @Client.on_message(filters.command("s"))
-async def sticker(client, message):
-    print("STICKER COMMAND RECEIVED")
-    await message.reply_text("✅ Sticker command working")
+async def stikcker(client, message):
+    await message.reply_text("Working")
